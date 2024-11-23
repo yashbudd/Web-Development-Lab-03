37300d24a77b38fcff051655a72e6a5ca10f1683
Sachi Goel <67666178+computer-s-2@users.noreply.github.com>
2024-11-22 21:23:41 -0500

    added stock page

---
+++ pages/3_Stock Prices.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import requests
+
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
+r = requests.get(url)
+data = r.json()
+
+print(data)
+
+# given text input: output 
