eacd6ba54d741e18a9561f518685b35428413603
yashbudd:git config --global user.name yashbuddgit config --global user.email yashbuddhdeo@gmail.com <135936758+yashbudd@users.noreply.github.com>
2024-11-22 21:35:23 -0500

    Update 3_Stock Prices.py
    Changed Stock API page

--- pages/3_Stock Prices.py
+++ pages/3_Stock Prices.py
@@ -8,3 +8,8 @@ data = r.json()
 print(data)
 
 # given text input: output 
+
+st.header("Stock Market Information")
+input = st.text_input("Choose a Symbol of a Current Stock")
+
+graph = st.radio("Choose on the following options", ["Open", "High", "Low", "Close", "Volume"])
