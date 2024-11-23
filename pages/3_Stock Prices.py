026b592058c564643458872f5e048a5f503fb2e4
Sachi Goel <67666178+computer-s-2@users.noreply.github.com>
2024-11-22 22:06:17 -0500

    worked on stock prices page

--- pages/3_Stock Prices.py
+++ pages/3_Stock Prices.py
@@ -1,17 +1,43 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 
 # given text input: output 
 
 st.header("Stock Market Information")
-input = st.text_input("Choose a Symbol of a Current Stock")
+input_var = st.text_input("Choose a Symbol of a Current Stock")
 
 graph = st.radio("Choose on the following options", ["Open", "High", "Low", "Close", "Volume"])
 
-try:
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={input}&apikey=demo'
-    r = requests.get(url)
-    data = r.json()
-except:
-    st.write("Enter a valid Stock Symbol (ex: IMB, APPL)")
+if st.button("Submit"):
+    if input_var:
+        api_key = "KDPU4BP0BEHBUVBT"
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={input_var}&apikey={api_key}'
+        r = requests.get(url)
+        data = r.json()
+        if "Time Series (Daily)" in data:
+            time_series = data["Time Series (Daily)"]
+
+            df = pd.DataFrame.from_dict(time_series, orient="index")
+            df = df.rename(columns={
+                "1. open": "Open",
+                "2. high": "High",
+                "3. low": "Low",
+                "4. close": "Close",
+                "5. volume": "Volume"
+            })
+
+            df.index = pd.to_datetime(df.index)
+            df = df.sort_index()
+            
+
+            st.write("Data fetched successfully!")
+            if graph == "Volume":
+                    st.line_chart(df["Volume"].astype(float), use_container_width=True)
+            else:
+                st.line_chart(df[graph].astype(float), use_container_width=True)
+        else:
+            st.error("Enter a valid Stock Symbol (e.g., IBM, AAPL).")
+    else:
+        st.warning("Please enter a stock symbol.")
