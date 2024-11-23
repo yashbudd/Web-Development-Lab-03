195c0b21ddf2b944b0059a0dc90fe56759b04b18
yashbudd <135936758+yashbudd@users.noreply.github.com>
2024-11-22 20:20:42 -0500

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab03")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Team 12, Web Development - Section C")
+st.subheader("Yash Buddhdeo, Sachi Goel")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
+
+1.
+2.
+3.
+4.
+
+""")
+
---
+++ Yash_info.py
@@ -0,0 +1,92 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/Yash.jpg"
+about_me = "Yash is a Freshman at the Georgia Institute of Technology, studying Computer Science with a concentration in System/Architecture and Intelligence. He is passionate and striving to contribute to the many AI / ML clubs on campus. When he was in high school, he was an active member in Student Government and FRC robotics. He is passionate about creating equitable change in his school and volunteering in his local community with IACS. Outside of school, he enjoys playing tennis, gaming, and hanging out with friends. During the winter months, he enjoys going to the slopes to ski and snowboard."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/yashbudd/"
+my_github_url = "https://github.com/yashbudd"
+my_email_address = "yashbuddhdeo@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Spring 2024',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 2050", "MATH 1554", "PSYC 3040", "CS 1100"], 
+    "names":["Intro to Computing", "Discrete Mathematics", "Linear Algebra", "Sensation & Perception", "Computer Science Seminar"], 
+    "semester_taken":["1st", "1st", "1st", "1st", "1st"],
+    "skills":["Python (Pandas, Streamlit), CSS, HTML, JavaScript", "Proof methods, algorithms over discrete structures", "Matrix operation, quadratic forms, eigenvectors", "How senses convert to computer vision and LLMs", "Interview preparation"],
+    }
+experience_data = {
+    "Algorithms Member of Propulsive Landers Student Research Team (Atlanta, GA, August 2024 - Present)": ([
+    "-Developing a positioning algorithm and dynamics model using Python, C++, PID control, and State-Space Methods",
+    "-Applying guidance, navigation, and control (GNC) principles to achieve successful hovering of 1.7 lb. Monoprop UAV", 
+    "-Curating a hybrid propulsion system to become first student team in the world to land the unmanned rocket"],"Images/PL.jpeg"),
+    "President/Mentor of FIRST Robotics Competition Team 2046 (Maple Valley, WA, September 2019 - August 2024)":([
+    "-Compiled Java programming of 4+ subsystems from intake to swerve drive modules using the WPILib FIRST extension",
+    "-Spearheaded 2-month progression of 90+ members from the design team on Autodesk Inventor to custom CNC fabrication",
+    "-Achieved a 2022 record of 68-17-0 and a 2023 record of 61-21-2 (17th out of 3317 global teams) with 10+ award wins"],"Images/BM.PNG"),
+    "Software Engineering Intern at Sobuso LLC (Seattle, WA, March 2022 - May 2022)":([
+    "-Engineered an e-commerce platform leading to over 20,000 units sold using full-stack development techniques", 
+    "-Wielded HTML, CSS, and JavaScript to create a friendly user interface and boost sustainable brand awareness", 
+    "-Applied data analysis to develop label designs for palm leaf tableware contributing to a 92 percent reduction in single-use plastic"],"Images/SWE.jpg")
+}
+
+projects_data = {
+    "Computer Vision Mask Detection System": "Trained a deep learning algorithm with a diverse dataset of 50,000+ images to allow for real-time mask detection. Architected a machine learning model with Amazon Rekognition and Python, achieving a 97 percent detection accuracy. Engaged with 20+ local businesses in Maple Valley, WA to promote public safety regulation during COVID-19.",
+}
+
+programming_data = {
+    "Python (Pandas, StreamLit)": 90,
+    "Java": 70,
+    "HTML": 75,
+    "CSS": 75,
+    "JavaScript": 60,
+    "SQL": 60,
+    "C++": 70
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "HTML": "🔍",
+    "CSS": "📀",
+    "JavaScript": "🤖",
+    "SQL": "🧑🏽‍💻",
+    "C++": "🛜"
+}
+spoken_icons = {"Hindi": "🇮🇳",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Hindi": "Native Fluency",
+    "Spanish": "Elementary Fluency",
+}
+leadership_data = {
+    "Partnerships Head of Georgia Tech Startup Exchange (Atlanta, GA, September 2024 - Present)": (["-Nurturing relationships with 850+ business contacts, actively looking for speakers and fundraising opportunities", "-Overseeing communication for Genesis, a 5-week entrepreneurship incubator, with 50+ members at weekly meetings", "-Promoting local business engagement for ShipIt, a weekly startup makerspace, with growing membership of 100+ leaders"],"Images/SX.jpeg"),
+    "Infrastructure and Security Committee Member of Georgia Tech Student Government Association (Atlanta, GA, September 2024 - Present)": (["-Attending weekly leadership training workshops with 25 leaders to develop soft skills in collaboration and career readiness", "-Fostering student culture within Georgia Tech by testing products from the IT Board using Python, Java, JavaScript, and SQL", "-Formulating plans for food, games, and relaxing activities for Spring Final Stand, projecting 2000+ Georgia Tech students"], "Images/SGA.jpeg")
+}
+activity_data={
+    "Youth Council Advisor/Youth Board Vice President of Indian American Community Services (Bellevue, WA, October 2020 - August 2024)": ([
+    "-Executed and oversaw 33+ service projects and led 3 5-day offsite cultural summer camps for 200+ kids", 
+    "-Created leadership and cultural lesson plans for 65 youth leaders and 32 camp counselors for bi-weekly meetings",
+    "-Awarded 3-time Gold President’s Volunteer Service Award (260 hours in 2023, 291 hours in 2022, 246 hours in 2021)"], "Images/IACS.jpg")
+}
